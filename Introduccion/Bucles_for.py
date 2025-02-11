b5d65d277eaec564bbc8cdd96e65c9bfb8ee89e9
0lmem <olmedilla.alvaro@gmail.com>
2025-02-11 12:19:30 +0100

    Cambio de nombre del archivo bucle.py y he agregado codigo

---
+++ Introduccion/Bucles_for.py
@@ -0,0 +1,11 @@
+'''
+ Ejemplo de bucle for:
+    1- Se define una variable llamada frase con el valor "Hola Mundo"
+    2- Se recorre la frase letra por letra
+    3- Se imprime cada letra
+'''
+
+frase = "Hola Mundo"
+
+for letra in frase:
+    print(letra)
