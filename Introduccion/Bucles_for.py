95b7d5293e5be2045a2505db166168647698c418
0lmem <olmedilla.alvaro@gmail.com>
2025-02-12 10:18:33 +0100

    Se ha agregado un ejemplo del for con range

--- Introduccion/Bucles_for.py
+++ Introduccion/Bucles_for.py
@@ -4,8 +4,22 @@
     2- Se recorre la frase letra por letra
     3- Se imprime cada letra
 '''
-
+print("Ejemplo de for: ")
 frase = "Hola Mundo"
 
 for letra in frase:
     print(letra)
+
+'''
+Ejemplo de bucle for con range:
+    1- Se define un bucle for con la funcion range
+    2- Se imprime el valor de i
+    3- La funcion range tiene la siguiente sintaxis:
+        range(inicio, fin, paso)
+        inicio: valor inicial
+        fin: valor final
+        paso: incremento
+'''
+print("Ejemplo de for con range: ")
+for i in range(1, 10, 2):
+    print(i)
