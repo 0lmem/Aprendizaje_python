d789380774adc647aa103ecd482e80e2c6df2a43
0lmem <olmedilla.alvaro@gmail.com>
2025-02-13 10:08:21 +0100

    Explicacion para cambiar elementos de una lista

--- Introduccion/Listas.py
+++ Introduccion/Listas.py
@@ -22,3 +22,19 @@ print(f"\nPrimer elemento de la lista: {lista_numerica[0]}")
 print(f"Muestra varios elementos de la lista: {lista_numerica[:3]}")
 print(f"Último elemento de la lista: {lista_numerica[-1]}")
 print(f"Muestra todos los elementos de la lista: {lista_numerica[:]}")
+
+'''
+Cambiar elementos de una lista:
+Para cambiar el valor de un elemento de una lista se utiliza el índice del elemento.
+'''
+
+print("\nCambiar elementos de una lista:")
+lista = ["a","e","i","o","u"]
+print(f"Lista original: {lista}")
+lista[0] = "A"
+print(f"Cambiamos la primera letra: {lista}")
+lista[1:3] = ["E", "I"]
+print(f"Modificamos de la posicion 1 a 3: {lista}")
+lista[:5] = ["a", "e", "I", "O"]
+print(f"Modificamos de la posicion 0 a 5: {lista}")
+
