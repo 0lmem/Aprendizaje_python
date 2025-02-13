27aff9fc52d085c4e9ac9a1b182f11d56f63f807
0lmem <olmedilla.alvaro@gmail.com>
2025-02-13 09:38:33 +0100

    Actualizacion del archivo listas.py

--- Introduccion/Listas.py
+++ Introduccion/Listas.py
@@ -7,10 +7,18 @@ y el primer elemento de la lista tiene el índice 0.
 print("Lista de números:")
 lista_numerica = [1, 2, 3, 4, 5]
 print(lista_numerica)
-# Acceder a un elemento de la lista
-print("accedemos a un elemento de la lista en este caso el primer elemento:")
-print(lista_numerica[0])
 
 print("\nLista de strings:")
 lista_strings = ["Hola", "Mundo", "Python"]
 print(lista_strings)
+
+'''
+Acceder a los elementos de una lista:
+Para acceder a los elementos de una lista se utiliza el índice del elemento.
+'''
+print("\nAcceder a elementos de una lista:")
+lista_numerica = [1, 2, 3, 4, 5]
+print(f"\nPrimer elemento de la lista: {lista_numerica[0]}")
+print(f"Muestra varios elementos de la lista: {lista_numerica[:3]}")
+print(f"Último elemento de la lista: {lista_numerica[-1]}")
+print(f"Muestra todos los elementos de la lista: {lista_numerica[:]}")
