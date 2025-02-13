9b2385e3740c830ec59647097e93070e01f0aad6
0lmem <olmedilla.alvaro@gmail.com>
2025-02-13 09:27:33 +0100

    Explicacion de las listas en python

---
+++ Introduccion/Listas.py
@@ -0,0 +1,16 @@
+'''
+Ejemplo de listas en Python:
+Las listas son un tipo de dato que nos permite almacenar varios valores en una sola variable.
+Las listas se crean utilizando corchetes [] y separando los elementos con comas.
+y el primer elemento de la lista tiene el índice 0.
+'''
+print("Lista de números:")
+lista_numerica = [1, 2, 3, 4, 5]
+print(lista_numerica)
+# Acceder a un elemento de la lista
+print("accedemos a un elemento de la lista en este caso el primer elemento:")
+print(lista_numerica[0])
+
+print("\nLista de strings:")
+lista_strings = ["Hola", "Mundo", "Python"]
+print(lista_strings)
